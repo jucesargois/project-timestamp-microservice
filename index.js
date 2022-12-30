42e6908e24bd9926029d4e8b3444fe73fa51f21d
Julio Cesar Gois <jucesargois@gmail.com>
2022-12-30 00:04:15 -0300

    Endpoint date modifications, inserted date validation and conversion

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-// index.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));  
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+const port = process.env.PORT || 8080;
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-app.post('/api', function (req, res, next) {
+app.post('/api/:date', function (req, res, next) {
+  const error = {"error": "Invalid Date"}
+  const resValid = {"date": ""}
+  const reqDate = req.params.date
+   
+  function dateIsValid(date) {
+    return date instanceof Date && !isNaN(date);
+  }
+  
+  if(dateIsValid(new Date(reqDate))){resValid.date = reqDate; res.json(resValid)}
+   else if(dateIsValid(new Date(reqDate * 1000))){
+    resValid.date = new Date(reqDate * 1000); res.json(resValid)
+   }else{
+    res.json(error)}
+   
+
   console.log(req.body)
-  res.json(req.body)
+
+  
 })
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
 });
